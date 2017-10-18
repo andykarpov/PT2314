1cf7cbf15edf908548b740842952c6efc6f552a9
tinvrban <tin.vrban@gmail.com>
2017-10-18 18:11:43 +0200

    Added the _gain variable and gain() method.

--- PT2314.h
+++ PT2314.h
@@ -42,18 +42,19 @@ private:
 	int _attenuationR;
 	bool _mute;
 	bool _loudness;
+	uint8_t _gain;
 	int _channel;
 	int _bass;
 	int _treble;
 	int volume_to_pt2314(int vol);
 	int eq_to_pt2314(int val);
-    int writeI2CChar(unsigned char c);
 	void updateVolume();
 	void updateAttenuation();
 	void updateAudioSwitch();
 	void updateBass();
 	void updateTreble();
 	void updateAll();
+	int writeI2CChar(unsigned char c);
     
 public:
     int init(void); 
@@ -63,10 +64,10 @@ public:
 	void channel(int ch);
 	void loudnessOn();
 	void loudnessOff();
+	void gain(int v);
 	void attenuation(int l, int r); 
     void bass(int b); 
     void treble(int t);
 };
 
 #endif
-
